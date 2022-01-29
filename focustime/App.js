a73c659b099e46e7e60c914cc8918bb763b496af
sahithimattapalli <msahithi98@gmail.com>
2022-01-29 18:56:28 -0500

    before navigation addition

--- focustime/App.js
+++ focustime/App.js
@@ -1,12 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Focus } from './src/features/focus/focus';
 import {Timer} from './src/features/timer/timer';
 import {fontSizes, paddingSizes, spacing} from "./src/utils/sizes";
+// import * as firebase from "firebase";
+
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAwzNIScPoR_diYOhB-SQIbonI7XQ2RXK4",
+//   authDomain: "focustime-a30ee.firebaseapp.com",
+//   projectId: "focustime-a30ee",
+//   storageBucket: "focustime-a30ee.appspot.com",
+//   messagingSenderId: "559315426645",
+//   appId: "1:559315426645:web:7015c27366c6618a58e3dc"
+// };
+
+// if(!firebase.apps.length){
+// firebase.initializeApp(firebaseConfig);
+//  }
 
 export default function App() {
+
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     firebase
+  //       .auth()
+  //       .signInWithEmailAndPassword("sahithi@mail.com", "test123")
+  //       .then((user) => {
+  //         console.log(user);
+  //         setIsAuthenticated(true);
+  //       })
+  //       .catch((e) => {
+  //         console.log(e);
+  //       });
+  //   }, 2000);
+  // }, []);
+
+
   const [focusSubject, setFocusSubject] = useState("gardening");
   return (
+    <>
     <View style={styles.container}>
       {focusSubject ? (
         <Timer focusSubject = {focusSubject} onTimerEnd={() => {
@@ -20,6 +55,7 @@ export default function App() {
       )}
       <Text>{focusSubject}</Text>
     </View>
+    </>
   );
 }
 
@@ -30,3 +66,4 @@ const styles = StyleSheet.create({
     paddingTop: spacing.md,
   },
 });
+
---
+++ focustime/src/service/authentication/authentication.service.js
@@ -0,0 +1,7 @@
+// import * as firebase from "firebase";
+
+// const loginRequest = (email, password) =>
+//   firebase.auth().signInWithEmailAndPassword(email, password);
+
+
+ 
