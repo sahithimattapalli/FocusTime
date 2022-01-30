9990cba5888b828273733a50281fe629a446ed78
sahithimattapalli <msahithi98@gmail.com>
2022-01-30 18:55:36 -0500

    focushistory

--- focustime/App.js
+++ focustime/App.js
@@ -6,9 +6,17 @@ import {fontSizes, paddingSizes, spacing} from "./src/utils/sizes";
 import { PermissionStatus } from 'expo-modules-core';
 import * as Notifications from 'expo-notifications';
 import { Notification } from 'expo-notifications';
+import { FocusHistory } from './src/features/focus/FocusHistory';
+
+
+const STATUSES = {
+  COMPLETE: 1,
+  CANCELLED: 2,
+};
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState("gardening");
+  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusHistory, setFocusHistory] = useState([]);
   const [notificationPermissions, setNotificationPermissions] = useState(
     PermissionStatus.UNDETERMINED,
   );
@@ -54,21 +62,37 @@ export default function App() {
     return () => listener.remove();
   }, [notificationPermissions]);
 
+// ///////////////////////
+const addFocusHistory = (subject, status) => {
+  setFocusHistory([...focusHistory, {key: String(focusHistory.length + 1),subject,status}]);
+};
+const onClear =() =>{
+setFocusHistory([]);
+}
+
 
   return (
     
     <View style={styles.container}>
       
       {focusSubject ? (
-        <Timer focusSubject = {focusSubject} onTimerEnd={() => {
-            setFocusSubject(null);
-          
+        <Timer focusSubject={focusSubject}
+        onTimerEnd={() => {
+          setFocusSubject(null);
+          addFocusHistory(focusSubject, STATUSES.COMPLETE);
         }}
-        clearSubject = {() => setFocusSubject(null)}
+          
+        clearSubject={() => {
+          addFocusHistory(focusSubject, STATUSES.CANCELLED);
+          setFocusSubject(null);
+        }}
         scheduleNotification = {scheduleNotification  }
         />
       ) : (
-        <Focus addSubject={setFocusSubject} />
+        <>
+          <Focus addSubject={setFocusSubject} />
+          <FocusHistory focusHistory = {focusHistory} onClear = {onClear}/>
+        </>
       )}
       <Text>{focusSubject}</Text>
     </View>
